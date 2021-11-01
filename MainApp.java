a7ad9cd7b207dbfbf503dc89d725dbecf9e7c3e2
Man <tranman731@gmail.com>
2021-11-01 15:47:43 +0700

    first commit

---
+++ MainApp.java
@@ -0,0 +1,35 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 50 ;
+		if (a ==9 );
+		int b = 5 ;
+		// Phép cộng
+		System.out.println("Tính tổng :" + (a+b));
+		//Phép trừ
+		System.out.println("Tính hiệu :" + (a-b));
+		//Phép nhân
+		System.out.println("Tính tích :" + (a*b));
+		//Phép chia
+		System.out.println("Tính thương :" + (a/b));
+		
+		int c = 15 ;
+		// So sánh 
+		System.out.println(c > a);
+        System.out.println(c > b);
+        System.out.println(c < a);
+        System.out.println(c < b);
+        System.out.println(c <= a);
+        System.out.println(c <= b);
+        System.out.println(c >= a);
+        System.out.println(c >= b);
+		
+	
+		
+
+	}
+
+}
